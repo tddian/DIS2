8e9aa9122700962c95dd685671f1fd28531286b7
Dian <judith1121@gmail.com>
2014-05-11 01:04:00 +0200

    in MyApp.java, add two other windows

--- assignment3/MyApp.java
+++ assignment3/MyApp.java
@@ -4,6 +4,12 @@ public class MyApp {
 		WindowManager windowSystem = new WindowManager(1000, 700);
 		SimpleWindow sw = windowSystem.createNewWindow();
 		windowSystem.moveWindow(sw,300,100);
+		
+		sw = windowSystem.createNewWindow();
+		windowSystem.moveWindow(sw,200,400);
+		
+		sw = windowSystem.createNewWindow();
+		windowSystem.moveWindow(sw,300,600);
 	}
 
 }
